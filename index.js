ddaa02605549c094e7ab21105888ac46a52667c7
jmnayea <koushik.ssv@gmail.com>
2024-04-25 23:21:31 +0530

    first

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
